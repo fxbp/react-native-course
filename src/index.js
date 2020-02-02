0fef54b45f2954c64a424acc52438a04a380b7eb
Francesc <fxbullich@gmail.com>
2020-02-02 11:54:00 +0100

    [FEAT] Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 
 import Routes from './routes'
+import Store from './redux/store'
 
-export default () => <Routes />
+const store = Store();
+
+export default () => (
+    <Provider store ={store} >
+        <Routes />  
+    </Provider>
+
+)
